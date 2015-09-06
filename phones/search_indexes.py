0345a26321a6f946dcd0d6958c6fff12d488405f
Sergey A. Kutylev <skutylev@hse.ru>
2015-09-06 23:22:09 +0300

    many changes

---
+++ phones/search_indexes.py
@@ -0,0 +1,15 @@
+from haystack import indexes
+from phones.models import Person, Unit, Position
+
+
+class PersonIndex(indexes.SearchIndex, indexes.Indexable):
+
+    text = indexes.SearchField(document=True, use_template=True)
+    content_auto = indexes.EdgeNgramField(model_attr='last_name')
+
+    def get_model(self):
+        return Person
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
