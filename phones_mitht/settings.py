da3a76618f53fd6d78c9b28fe3c5fdc9930c0d67
Sergey A. Kutylev <skutylev@hse.ru>
2015-09-06 23:12:03 +0300

    many changes

--- phones_mitht/settings.py
+++ phones_mitht/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = normpath(join(BASE_DIR, 'static', 'collected'))
 
