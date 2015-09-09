dd5b697732a003ef64df3653928ebff2dc1686fb
Sergey A. Kutylev <skutylev@hse.ru>
2015-09-09 12:53:53 +0300

    many changes

--- phones/models.py
+++ phones/models.py
@@ -24,7 +24,7 @@ def get_person_image_path(instance, filename):
 # Добавляем модели данных #
 ###########################
 
-# Телефонные префиксы
+
 class Prefix(models.Model):
     prefix = models.CharField(max_length=9, verbose_name='Префикс', default='1')
 
@@ -36,7 +36,7 @@ class Prefix(models.Model):
         verbose_name_plural = 'Префиксы'
         ordering = ['prefix']
 
-# Коды города
+
 class AreaCode(models.Model):
     area_code = models.CharField(max_length=6, verbose_name='Код города', default='1')
 
@@ -48,7 +48,7 @@ class AreaCode(models.Model):
         verbose_name_plural = 'Коды городов'
         ordering = ['area_code']
 
-# Почтовые индексы
+
 class PostCode(models.Model):
     post_code = models.CharField(max_length=6, verbose_name='Почтовый индекс', default='1')
 
@@ -59,7 +59,8 @@ class PostCode(models.Model):
         verbose_name = 'Почтовый индекс'
         verbose_name_plural = 'Почтовые индексы'
         ordering = ['post_code']
-# Города
+
+
 class City(models.Model):
     city = models.CharField(max_length=10, verbose_name='Город', default='1')
     area_code = models.ManyToManyField(AreaCode, verbose_name="Код города", default='1')
@@ -72,7 +73,7 @@ class City(models.Model):
         verbose_name_plural = 'Города'
         ordering = ['city']
 
-# Улицы
+
 class Street(models.Model):
     street = models.CharField(max_length=40, verbose_name='Улица', default='1')
     post_code = models.ManyToManyField(PostCode, verbose_name="Почтовый индекс", default='1')
@@ -85,7 +86,7 @@ class Street(models.Model):
         verbose_name_plural = 'Улицы'
         ordering = ['street']
 
-# Здания
+
 class Building(models.Model):
     building = models.CharField(max_length=5, verbose_name='Здание', default='1')
 
@@ -97,7 +98,7 @@ class Building(models.Model):
         verbose_name_plural = 'Здания'
         ordering = ['building']
 
-# Корпуса в зданиях
+
 class Campus(models.Model):
     campus = models.CharField(max_length=5, verbose_name='Корпус', default='1')
     prefix = models.ManyToManyField(Prefix, verbose_name='Префикс', default='1')
@@ -110,7 +111,7 @@ class Campus(models.Model):
         verbose_name_plural = 'Корпуса'
         ordering = ['campus']
 
-# Кабинеты
+
 class Office(models.Model):
     office = models.CharField(max_length=5, verbose_name='Кабинет')
 
@@ -122,7 +123,7 @@ class Office(models.Model):
         verbose_name_plural = 'Кабинеты'
         ordering = ['office']
 
-# Часы работы
+
 class WorkHours(models.Model):
     begin_hours = models.TimeField(verbose_name='Начало рабочего дня')
     end_hours = models.TimeField(verbose_name='Конец рабочего дня')
@@ -135,7 +136,7 @@ class WorkHours(models.Model):
         verbose_name_plural = 'Часы работы'
         ordering = ['begin_hours']
 
-# Телефоны
+
 class Phone(models.Model):
     country_code = models.CharField(max_length=2, verbose_name='Код страны', default='+7')
     area_code = models.ForeignKey(AreaCode)
@@ -150,7 +151,7 @@ class Phone(models.Model):
         verbose_name_plural = 'Телефоны'
         ordering = ['area_code']
 
-# Адреса
+
 class Address(models.Model):
     city = models.ForeignKey(City)
     street = models.ForeignKey(Street)
@@ -166,7 +167,7 @@ class Address(models.Model):
         verbose_name_plural = 'Адреса'
         ordering = ['street']
 
-# Образование
+
 class Edu(models.Model):
     EDU_CHOICES = (
         ('Бакалавриат', 'Бакалавриат'),
@@ -191,7 +192,7 @@ class Edu(models.Model):
         verbose_name = 'Образование'
         verbose_name_plural = 'Образования'
 
-# Ученые степени
+
 class Degree(models.Model):
     degree = models.CharField(max_length=50, verbose_name='Ученая степень')
     short_degree = models.CharField(max_length=6, verbose_name='Сокращение')
@@ -203,7 +204,7 @@ class Degree(models.Model):
         verbose_name = 'Ученая степень'
         verbose_name_plural = 'Ученые степени'
 
-# Ученые звания
+
 class ScienceRank(models.Model):
     science_rank = models.CharField(max_length=50, verbose_name='Ученое звание')
     short_science_rank = models.CharField(max_length=6, verbose_name='Сокращение')
@@ -215,7 +216,7 @@ class ScienceRank(models.Model):
         verbose_name = 'Ученое звание'
         verbose_name_plural = 'Ученые звания'
 
-# Справочник организаций
+
 class Organization(models.Model):
     org_name = models.CharField(max_length=100, verbose_name='Название организации')
     short_org_name = models.CharField(max_length=10, verbose_name='Короткое название')
@@ -232,7 +233,7 @@ class Organization(models.Model):
         verbose_name_plural = 'Организации'
         ordering = ['org_name']
 
-# Справочник подразделений, с шифрами и иерархией
+
 class Unit(MPTTModel, models.Model):
     unit_cypher = models.CharField(max_length=15, verbose_name='Шифр', blank=True)
     unit_name = models.CharField(max_length=200, verbose_name='Подразделение')
@@ -262,7 +263,7 @@ class Unit(MPTTModel, models.Model):
 
 mptt.register(Unit,)
 
-# Справочник должностей, с начальниками и подчиненными
+
 class Position(models.Model):
     position = models.CharField(max_length=100, verbose_name='Должность')
 
@@ -274,7 +275,7 @@ class Position(models.Model):
         verbose_name_plural = 'Должности'
         ordering = ['position']
 
-# И наконец люди
+
 class Person(models.Model):
     user = models.OneToOneField(User, verbose_name='Пользователь', blank=True, null=True, )
     last_name = models.CharField(max_length=30, verbose_name='Фамилия')
@@ -282,8 +283,7 @@ class Person(models.Model):
     middle_name = models.CharField(max_length=30, verbose_name='Отчество')
     birthday = models.DateField(verbose_name='Дата рождения', blank=True, default='00.00.0000')
     email = models.EmailField(verbose_name='Email', blank=True)
-    photo = models.ImageField(upload_to=get_person_image_path, verbose_name='Фотография', blank=True, default=None
-                              )
+    photo = models.ImageField(upload_to=get_person_image_path, verbose_name='Фотография', blank=True, default=None)
     slug = models.SlugField(max_length=30, verbose_name='Ссылка', blank=True)
     unit = models.ManyToManyField(Unit, verbose_name='Подразделение', related_name='units')
     position = models.ManyToManyField(Position, verbose_name='Должность', related_name='positions', blank=True, null=True)
